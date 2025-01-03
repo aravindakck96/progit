b755e95b805a59c39c8b91788db8c74588b3ac5f
aravindakck96 <158430927+aravindakck96@users.noreply.github.com>
2025-01-03 15:23:34 +0530

    aa
    aa

---
+++ src/main/java/com/asolutions/progit/ProgitApplication.java
@@ -0,0 +1,13 @@
+package com.asolutions.progit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asolutions/progit/ProgitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asolutions.progit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
