d03fd2007eff320f10d7d1673e66cfcbda309855
aravindakck96 <158430927+aravindakck96@users.noreply.github.com>
2025-01-03 15:27:23 +0530

    mod
    aaa

--- src/main/java/com/asolutions/progit/entity/git.java
+++ src/main/java/com/asolutions/progit/entity/git.java
@@ -3,4 +3,8 @@ package com.asolutions.progit.entity;
 public class git {
     private int id;
     private String name;
+
+    private String url;
+    private String commit;
+    private String author;
 }
